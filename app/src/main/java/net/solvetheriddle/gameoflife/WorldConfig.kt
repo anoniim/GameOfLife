eef814a1fb484c34f82b8d97d46b4e705a557f8a
Marcel Broz <marcel.broz@callsign.com>
2018-09-28 22:55:33 +0100

    WorldViewModel
    Signed-off-by: Marcel Broz <marcel.broz@callsign.com>

--- app/src/main/java/net/solvetheriddle/gameoflife/WorldConfig.kt
+++ app/src/main/java/net/solvetheriddle/gameoflife/WorldConfig.kt
@@ -2,9 +2,10 @@ package net.solvetheriddle.gameoflife
 
 interface WorldConfig {
     companion object {
-        const val LINE_WIDTH = 2
-        const val MIN_CELL_SIZE = 5
-        const val MIN_WORLD_SIZE = 10
+        const val LINE_WIDTH = 2 // Grid line width in pixels
+        const val MIN_CELL_SIZE = 5 // Cell square size in pixels
+        const val MIN_WORLD_SIZE = 10 // Number of cells on the smaller dimension
+        const val AUTO_ZOOM_AMOUNT = 0.2f // Factor of automatic zoom step
     }
 
 }
